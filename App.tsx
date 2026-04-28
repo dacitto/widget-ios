889d404acef69f5aaff4b4b7a06fadb2fe869a64
dacitto <dacidigital@gmail.com>
2026-04-28 16:11:48 +0100

    Xcode > Update widget after writing count to shared file

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { requireNativeModule } from "expo-modules-core";
+import { useEffect } from "react";
+
+const Counter = requireNativeModule("counter");
 
 export default function App() {
+  useEffect(() => {
+    Counter.setCount(10);
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
