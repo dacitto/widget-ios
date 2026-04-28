38c0f56588f39c4622e266396ca8ef4095d9ea78
dacitto <dacidigital@gmail.com>
2026-04-28 10:44:49 +0100

    Xcode > Display fixed count in widget

--- ios/CounterWidget/CounterWidget.swift
+++ ios/CounterWidget/CounterWidget.swift
@@ -45,13 +45,8 @@ struct CounterWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Favorite Emoji:")
-            Text(entry.configuration.favoriteEmoji)
-        }
+      Text("5")
+             .font(.largeTitle)
     }
 }
 
